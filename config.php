ce8f00ba2ebd5428056b7d0409c7939d9c3b603e
evgenii <jeka-luda@yandex.ru>
2017-07-03 18:39:12 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://test.local/shopgiro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://test.local/shopgiro/');
+
+// DIR
+define('DIR_APPLICATION', '/Users/Evgeny/www/test.local/shopgiro/catalog/');
+define('DIR_SYSTEM', '/Users/Evgeny/www/test.local/shopgiro/system/');
+define('DIR_IMAGE', '/Users/Evgeny/www/test.local/shopgiro/image/');
+define('DIR_LANGUAGE', '/Users/Evgeny/www/test.local/shopgiro/catalog/language/');
+define('DIR_TEMPLATE', '/Users/Evgeny/www/test.local/shopgiro/catalog/view/theme/');
+define('DIR_CONFIG', '/Users/Evgeny/www/test.local/shopgiro/system/config/');
+define('DIR_CACHE', '/Users/Evgeny/www/test.local/shopgiro/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Users/Evgeny/www/test.local/shopgiro/system/storage/download/');
+define('DIR_LOGS', '/Users/Evgeny/www/test.local/shopgiro/system/storage/logs/');
+define('DIR_MODIFICATION', '/Users/Evgeny/www/test.local/shopgiro/system/storage/modification/');
+define('DIR_UPLOAD', '/Users/Evgeny/www/test.local/shopgiro/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shopgiro');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
